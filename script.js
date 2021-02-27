73cf025fa494a9923b2c0af50fcd049028d35051
Morgan Schall <morgan.allison.schall@gmail.com>
2021-02-27 18:49:55 -0500

    got the HTML documents to appear on the screen as I want them.

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-var currentTime = moment().format('MMMM Do YYYY');
+
+var current= moment().format('MMMM Do YYYY');
 // we need to use moment to reflect what day it is to the user to plan their day
 var currentDay = $('#currentDay');
 //defining the HTML variable in JavaScript so I can apply moment to change the dates 
-currentDay.append(currentTime);
+currentDay.append(current);
 //appending the moment formatting and current time display to that HTML emlement
 
 
-var hourFormat = moment().format('h a');
+
+$(document).ready(function() {
+
+
+
+function timeKeeper(){
+    var currentTime = moment().hours;
+    $(".time-block").each(function(){
+        var hour = parseInt($(this).attr("id"));
+        if(hour < currentTime){
+            $(this).addClass("past")
+        } else if(hour = currentTime){
+            $(this).addClass("present")
+            $(this).removeClass("past")
+            $(this).removeClass("future")
+        } else
+            $(this).addClass("future")
+            $(this).removeClass("past")
+            $(this).removeClass("present")
+    });
+    }   
+
+    timeKeeper();
+
+});

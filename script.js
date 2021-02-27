1832b2fee8401c95dffdd778fc9db83fedf02854
Morgan Schall <morgan.allison.schall@gmail.com>
2021-02-27 15:19:02 -0500

    tried adding hard coded HTML elements that I manipulate with java script instead of using a loop to create the calendar blocks

--- script.js
+++ script.js
@@ -6,21 +6,34 @@ var currentDay = $('#currentDay');
 currentDay.append(currentTime);
 //appending the moment formatting and current time display to that HTML emlement
 
+var row = $("tr");
+row.addClass("row");
 
-var timeBlocks = $(".container");
-//there was a pre defined container where our time blocks need to go 
-var table = $('<table>')
-timeBlocks.append(table);
-//in office hours our instructor did a little demo on how to get time blocks to add using query 
-//for neater code we assigned that element to a variable which I appended to the HTML 
-var workingHours =[];
-//this is an array that holds the hours we worked 
-for(i=9; i<18; i++ ){
-    var tr = $('<tr>');
-    var td = $('<td>');
-    td.text(i);
-    tr.append(td);
-    table.append(tr);
-}
+
+// var timeBlocks = $(".container");
+// //there was a pre defined container where our time blocks need to go 
+// var table = $('<table>')
+// timeBlocks.append(table);
+// //in office hours our instructor did a little demo on how to get time blocks to add using query 
+// //for neater code we assigned that element to a variable which I appended to the HTML 
+
+
+
+// for(hour=9; hour<=12; hour++ ){
+//     var tr = $('<tr>');
+//     var td = $('<td>');
+//     td.text(hour + " AM");
+//     td.addClass('hour')
+//     tr.append(td);
+//     table.append(tr);
+// }
+// for(hour=1; hour<=5; hour++ ){
+//     var tr = $('<tr>');
+//     var td = $('<td>');
+//     td.text(hour + " PM");
+//     tr.append(td);
+//     table.append(tr);
+// }
 // this loop was also demoed and we create a for loop to add blocks to the HTML
-// i is equal to the 9-5 hours of work 
+// hour is equal to the 9-5 hours of work 
+

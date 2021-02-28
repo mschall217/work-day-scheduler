def8ce207e09ea81cb8d0fd5ad6d76d13d278233
Morgan Schall <morgan.allison.schall@gmail.com>
2021-02-27 20:17:37 -0500

    THE TIME VS COLOR OF BLOCK IS WORKINGgit add .

--- script.js
+++ script.js
@@ -1,34 +1,46 @@
-
-var current= moment().format('MMMM Do YYYY');
-// we need to use moment to reflect what day it is to the user to plan their day
-var currentDay = $('#currentDay');
-//defining the HTML variable in JavaScript so I can apply moment to change the dates 
-currentDay.append(current);
-//appending the moment formatting and current time display to that HTML emlement
-
-
-
 $(document).ready(function() {
 
+    var currentDate= moment().format('dddd, MMMM Do YYYY');
+    // we need to use moment to reflect what day it is to the user to plan their day
+    var currentDay = $('#currentDay');
+    //defining the HTML variable in JavaScript so I can apply moment to change the dates 
+    currentDay.append(currentDate);
+    //appending the moment formatting and current time display to that HTML emlement
+
+    var nine = $('#9');
+    var ten = $('#10');
+    var eleven = $('#11');
+    var noon = $('#12');
+    var one = $('#13');
+    var two = $('#14');
+    var three = $('#15');
+    var nine = $('#16');
+    var nine = $('#17');
+
+    
+    var rightNow = parseInt(moment().format('HH'));
+    console.log(rightNow)
+     $("textarea").each(function(){
+        var index = parseInt($(this).attr("index"));
+         console.log(index);
+         if( index < rightNow){
+            $(this).addClass("past");
+            return;
+         } 
+         if(index > rightNow){
+            $(this).addClass("future");
+            return;
+         }
+         if(index === rightNow){
+            $(this).addClass("present");
+            return;
+         }
+     });
 
 
-function timeKeeper(){
-    var currentTime = moment().hours;
-    $(".time-block").each(function(){
-        var hour = parseInt($(this).attr("id"));
-        if(hour < currentTime){
-            $(this).addClass("past")
-        } else if(hour = currentTime){
-            $(this).addClass("present")
-            $(this).removeClass("past")
-            $(this).removeClass("future")
-        } else
-            $(this).addClass("future")
-            $(this).removeClass("past")
-            $(this).removeClass("present")
-    });
-    }   
 
-    timeKeeper();
 
-});
+
+
+
+     })   
